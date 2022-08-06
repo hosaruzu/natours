114ff998ddf3c865f6f12c05b8613f52e6920d1a
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-08-06 20:16:33 +0400

    refactor: rebase project architecture

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   rules: {
     'selector-class-pattern': null,
     'keyframes-name-pattern': null,
+    'at-rule-no-unknown': null,
     /* only for empty *.scss files
       TODO: remove no-empty-sourse after fill unused *.scss */
     'no-empty-source': null,
